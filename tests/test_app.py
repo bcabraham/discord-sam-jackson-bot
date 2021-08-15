7ae99871f6d579763e57ffcea6689193aca14156
bcabraham <abraham.bryan@gmail.com>
2021-08-14 22:09:24 -0600

    added test_get_help_message

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import replace_all, repl_dict
+from src.app import replace_all, repl_dict, get_help_message
 
 
 def test_replace_all():
@@ -6,3 +6,15 @@ def test_replace_all():
     result = replace_all(text, repl_dict)
 
     assert result == "motherfucker, this is a motherfucking test"
+
+
+def test_get_help_message():
+    result = get_help_message()
+
+    all_match = True
+    for k, v in repl_dict.items():
+        if k not in result or v not in result:
+            all_match = False
+            break
+
+    assert all_match
