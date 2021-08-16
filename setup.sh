a1c06425f566659f0576f73608e2ca5807a3fdf8
John Casillas <jhc@i3s-macbook-pro.lan>
2021-08-15 20:44:46 -0600

    updated setup.sh to include a docker install I tested on the server

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo amazon-linux-extras install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh -y
 chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ubuntu
